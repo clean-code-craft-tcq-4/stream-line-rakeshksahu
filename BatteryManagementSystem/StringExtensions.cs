37dbccb1c56fdc8a99c4b4ff9a5ff96db9344e36
ramandhiman6243 <73428072+ramandhiman6243@users.noreply.github.com>
2022-09-19 20:32:01 +0530

    Added receiver source files

---
+++ BatteryManagementSystem/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace BatteryManagementSystem
+{
+    public static class StringExtensions
+    {
+        public static string TrimString(this string inputString, int startIndex, int endIndex)
+        {
+            int length = CalculateLength(startIndex, endIndex);
+            string substring = inputString.Substring(startIndex, length);
+            return substring;
+        }
+
+        static int CalculateLength(int startIndex, int endIndex)
+        {
+            int length = endIndex - startIndex + 1;
+            return length;
+        }
+
+        public static double[] CsvToDoubleArray(this string csv, string separator)
+        {
+            string[] stringValues = csv.Split(separator);
+            double[] doubleValues = StringArrayToDoubleArray(stringValues);
+            return doubleValues;
+        }
+
+        private static double[] StringArrayToDoubleArray(string[] stringValues)
+        {
+            double[] doubleValues = new double[stringValues.Length];
+            for (int i = 0; i < stringValues.Length; i++)
+            {
+                doubleValues[i] = double.Parse(stringValues[i]);
+            }
+            return doubleValues;
+        }
+
+        public static bool Contains(this string input, params char[] chars)
+        {
+            foreach (var _char in chars)
+            {
+                if (!input.Contains(_char))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ BatteryManagementSystemTests/ReceiverConversionTests.cs
@@ -0,0 +1,106 @@
+using BatteryManagementSystem;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BatteryManagementSystemTests
+{
+    public class ReceiverConversionTests
+    {
+        void TestParameterConversion(string senderOutput, List<BatteryParameter> expectedParameters)
+        {
+            List<BatteryParameter> actualOutput = Receiver.ConvertInputToParameterList(senderOutput);
+            AssertEquality(expectedParameters, actualOutput);
+        }
+
+        void AssertEquality(List<BatteryParameter> expectedOutput, List<BatteryParameter> actualOutput)
+        {
+            //Check equal size
+            Assert.Equal(expectedOutput.Count, actualOutput.Count);
+
+            //Check distint elements
+            for (int i = 0; i < expectedOutput.Count; i++)
+            {
+                var expectedItem = expectedOutput[i];
+                var actualItem = actualOutput[i];
+                AssertEquality(expectedItem, actualItem);
+            }
+        }
+
+        void AssertEquality(BatteryParameter expectedParameter, BatteryParameter actualParameter)
+        {
+            Assert.Equal(expectedParameter.Name, actualParameter.Name);
+            Assert.Equal(expectedParameter.Values, actualParameter.Values);
+        }
+
+        [Fact]
+        public void SingleParameterConversionTest()
+        {
+            string senderOutput = ReceiverTestConstants.SingleParametersInput;
+
+            List<BatteryParameter> expectedOutput = new List<BatteryParameter>
+            {
+                new BatteryParameter()
+                {
+                    Name = "Temperature readings",
+                    Values = new double[]{ 38, 18, 35, 36, 41, 8.9, 15, 35, 12, 25 }
+                }
+            };
+
+            TestParameterConversion(senderOutput, expectedOutput);
+        }
+
+        [Fact]
+        public void MultipleParametersConversionTest()
+        {
+            string senderOutput = ReceiverTestConstants.MultipleParametersInput;
+
+            List<BatteryParameter> expectedOutput = new List<BatteryParameter>
+            {
+                new BatteryParameter()
+                {
+                    Name = "Temperature readings",
+                    Values = new double[]{ 38, 18, 35, 36, 41, 8.9, 15, 35, 12, 25 }
+                },
+                new BatteryParameter()
+                {
+                    Name = "SOC readings",
+                    Values = new double[]{ 49, 58, 42, 51, 77, 75, 58, 63, 28, 56 }
+                },
+                new BatteryParameter()
+                {
+                    Name = "Charge Rate readings",
+                    Values = new double[]{ 0.013, 0.19, 0.11, 0.64, 0.13, 0.32, 0.1, 0.087, 0.8, 0.17 }
+                }
+            };
+
+            TestParameterConversion(senderOutput, expectedOutput);
+        }
+
+        [Fact]
+        public void InvalidInputConversionTest()
+        {
+            string senderOutput = ReceiverTestConstants.InvalidInput1;
+            List<BatteryParameter> expectedOutput = new List<BatteryParameter>();
+            TestParameterConversion(senderOutput, expectedOutput);
+
+            senderOutput = ReceiverTestConstants.InvalidInput2;
+            TestParameterConversion(senderOutput, expectedOutput);
+        }
+
+        [Fact]
+        public void EmptyInputConversionTest()
+        {
+            string senderOutput = string.Empty;
+            List<BatteryParameter> expectedOutput = new List<BatteryParameter>();
+            TestParameterConversion(senderOutput, expectedOutput);
+        }
+
+        [Fact]
+        public void NullInputConversionTest()
+        {
+            string senderOutput = null;
+            List<BatteryParameter> expectedOutput = new List<BatteryParameter>();
+            TestParameterConversion(senderOutput, expectedOutput);
+        }
+    }
+}
