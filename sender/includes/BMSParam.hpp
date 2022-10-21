9d826d85bc2c9d52681518ec92524675bf3416d1
Sahu, Rakesh Kumar (BSH) <sahu.rakesh-ext@bshg.com>
2022-10-21 12:38:08 +0530

    added function headers and comments

--- sender/includes/BMSParam.hpp
+++ sender/includes/BMSParam.hpp
@@ -8,38 +8,81 @@
 
 using namespace std;
 
+/******************************************************************************************************************
+ *
+ * @brief This interface is responsible for generating parameter values based on number of reading required
+ *
+ ******************************************************************************************************************/
 class BMSParam
 {
     public:
     BMSParam() = default;
     virtual ~BMSParam();
+    /******************************************************************************************************************
+	 * @brief this method is used to generate readings
+	 *
+	 * @param : numberOfReadings : parameter holding number of readings required
+	 *
+	 * @return  vector holding generated readings
+	 *
+	 ******************************************************************************************************************/
     virtual vector<float> generateValues(uint32_t numberOfReadings) = 0;
 };
 
+/******************************************************************************************************************
+ *
+ * @brief This class is responsible for generating temperature values based on number of reading required
+ *
+ ******************************************************************************************************************/
 class Temperature : public BMSParam
 {
     public:
     vector<float> generateValues(uint32_t numberOfReadings) override;
+    
     private:
+    
+    /* @brief member specifying Minimum temperature value.*/
     const float MIN_TEMP {0};
+
+    /* @brief member specifying Maximum temperature value.*/
     const float MAX_TEMP {45};
 };
 
+/******************************************************************************************************************
+ *
+ * @brief This class is responsible for generating state of charge values based on number of reading required
+ *
+ ******************************************************************************************************************/
 class SOC : public BMSParam
 {
     public:
     vector<float> generateValues(uint32_t numberOfReadings) override;
+
     private:
+
+    /* @brief member specifying Minimum soc value.*/
     const float MIN_SOC {20};
+
+    /* @brief member specifying Maximum soc value.*/
     const float MAX_SOC {80};
 };
 
+/******************************************************************************************************************
+ *
+ * @brief This class is responsible for generating charge rate values based on number of reading required
+ *
+ ******************************************************************************************************************/
 class ChargeRate : public BMSParam
 {
     public:
     vector<float> generateValues(uint32_t numberOfReadings) override;
+
     private:
+
+    /* @brief member specifying Minimum charge rate value.*/
     const float MIN_CHARGE_RATE {0};
+
+    /* @brief member specifying Maximum charge rate value.*/
     const float MAX_CHARGE_RATE {.8};
 };
 
--- test_sender.cpp
+++ test_sender.cpp
@@ -1,6 +1,22 @@
+
+//#define CATCH_CONFIG_MAIN 
+//#include "test/catch.hpp"
+//#include <assert.h>
+
 #include "sender/includes/sender.hpp"
 using namespace std;
 
+
+/*
+//disabling test cases to print required content for receiver on console
+TEST_CASE("test set 1 - check the output for invalid number of readings") {
+  REQUIRE(sendSensorDataToConsole(0));
+}
+
+TEST_CASE("test set 2 - check the output for valid number of readings") {
+  REQUIRE(sendSensorDataToConsole(10));
+}
+*/
 
 int main ()
 {
